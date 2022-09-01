4454439ba29704c21c38e4d348e6a5637f56be2d
omarashzeinhom <omar.ashraf.zeinhom@hotmail.com>
2022-09-01 10:49:57 +0100

    added-fix-

--- adminpanel/manage_categories.php
+++ adminpanel/manage_categories.php
@@ -15,121 +15,118 @@ $categories = mysqli_query($connect__db, $category__db__query);
 
     <?php
     if (isset($_SESSION['user_is_admin'])) : ?>
-    <ul>
-        <li class='app__sidenav-item'><a href="<?= ADMIN_URL ?>add_category.php" class="app__link-btn">Add
-                Category✨</a>
-        </li>
+        <ul>
+            <li class='app__sidenav-item'><a href="<?= ADMIN_URL ?>add_category.php" class="app__link-btn">Add
+                    Category✨</a>
+            </li>
 
 
-    </ul>
+        </ul>
 
-    <!-- Alert success start -->
+        <!-- Add Category Alert success start -->
 
-    <?php if (isset($SESSION_['add-category-success'])) : ?>
-    <div class="app__alert-success">
-        <p class="app__alert-success-p">
-            <?= $_SESSION['add-category-success'];
+        <?php if (isset($SESSION_['add-category-success'])) : ?>
+            <div class="app__alert-success">
+                <p class="app__alert-success-p">
+                    <?= $_SESSION['add-category-success'];
                     unset($_SESSION['add-category-success']); ?>
-        </p>
-    </div>
+                </p>
+            </div>
+            <!-- Add Category Alert success End  -->
 
-    <?php elseif (isset($_SESSION['add-category'])) : ?>
-    <div class="app__alert-error">
-        <p class="app__alert-p">
-            <?= $_SESSION['add-category'];
+            <!-- Add Category Alert Failed  start -->
+
+        <?php elseif (isset($_SESSION['add-category'])) : ?>
+            <div class="app__alert-error">
+                <p class="app__alert-p">
+                    <?= $_SESSION['add-category'];
                     unset($_SESSION['add-category']); ?>
 
-        </p>
-    </div>
+                </p>
+            </div>
+            <!-- Add Category Alert Failed  End -->
 
-    <!--- IF EDIT CATEGORY WAS SUCCESFULL-->
+            <!--- IF EDIT CATEGORY WAS SUCCESFULL-->
 
-    <?php elseif (isset($_SESSION['edit-category-success'])) : ?>
-    <div class="app__alert-success">
-        <p class="app__alert-success-p">
-            <?= $_SESSION['edit-category-success'];
+        <?php elseif (isset($_SESSION['edit-category-success'])) : ?>
+            <div class="app__alert-success">
+                <p class="app__alert-success-p">
+                    <?= $_SESSION['edit-category-success'];
                     unset($_SESSION['edit-category-success']); ?>
 
-        </p>
-    </div>
-    <!--- IF EDIT CATEGORY WAS NOT SUCCESFULL-->
-    <?php elseif (isset($_SESSION['edit-category'])) : ?>
-    <div class="app__alert-error">
-        <p class="app__alert-p">
-            <?= $_SESSION['edit-category'];
+                </p>
+            </div>
+            <!--- IF EDIT CATEGORY WAS NOT SUCCESFULL-->
+        <?php elseif (isset($_SESSION['edit-category'])) : ?>
+            <div class="app__alert-error">
+                <p class="app__alert-p">
+                    <?= $_SESSION['edit-category'];
                     unset($_SESSION['edit-category']); ?>
 
-        </p>
-    </div>
-    <!--- IF DELETE CATEGORY WAS  SUCCESFULL-->
-    <?php elseif (isset($_SESSION['delete-category-success'])) : ?>
-    <div class="app__alert-success">
-        <p class="app__alert-success-p">
-            <?= $_SESSION['delete-category-success'];
+                </p>
+            </div>
+            <!--- IF DELETE CATEGORY WAS  SUCCESFULL-->
+        <?php elseif (isset($_SESSION['delete-category-success'])) : ?>
+            <div class="app__alert-success">
+                <p class="app__alert-success-p">
+                    <?= $_SESSION['delete-category-success'];
                     unset($_SESSION['delete-category-success']); ?>
 
-        </p>
-    </div>
+                </p>
+            </div>
+        <?php endif; ?>
+        <!-- Alert success End -->
+
+        <!-- Categories LESS THAN than 0 start -->
+
+        <?php if (mysqli_num_rows($categories) > 0) : ?>
+            <!--- Table and While Loop Start -->
+            <div class="container" style="overflow: x-auto;">
+                <table class="app__table">
+                    <thead class="app__thead">
+
+                        <tr clas="app__tr">
+                            <th class="app_th">Title</th>
+                            <th class="app_th">Description</th>
+                            <th class="app_th">Edit</th>
+                            <th class="app_th">Delete</th>
+
+                        </tr>
+
+                    </thead>
 
 
-    <?php endif; ?>
-    <!-- Alert success End -->
-
-    <!-- Categories LESS THAN than 0 start -->
-
-    <?php if (mysqli_num_rows($categories) > 0) : ?>
-    <!--- Table and While Loop Start -->
-    <div class="container" style="overflow: x-auto;">
-        <table class="app__table">
-            <thead class="app__thead">
-
-                <tr clas="app__tr">
-                    <th class="app_th">Title</th>
-                    <th class="app_th">Description</th>
-                    <th class="app_th">Edit</th>
-                    <th class="app_th">Delete</th>
-
-                </tr>
-
-            </thead>
-            <tbody class="app__tbody">
-                <!---LOOP THROUGH AND DISPLAY USER -->
-                <?php while ($categories = mysqli_fetch_assoc(($categories))) : ?>
-                <tr clas="app__tr">
-                    <td class="app__td"><?= $categories['title']  ?></td>
-                    <td class="app__td"><?= $categories['description'] ?? null ?></td>
-                    <td class="app__td"><a href="<?= ADMIN_URL ?>edit_category.php?id=<?= $categories['id'] ?>"
-                            class="app__link-btn">Edit</a></td>
-                    <td class="app__td"><a href="<?= ADMIN_URL ?>delete_category.php?id=<?= $categories['id'] ?>"
-                            class="app__alert-btn-sm">Delete</a>
-                    </td>
-                </tr>
-                <?php endwhile; ?>
-            </tbody>
-        </table>
-    </div>
-    <?php else : ?>
-
-    <div class="app__alert-error">
-        <p class="app__alert-p">
-            <?= " No Catgories Found !" ?>
-
-        </p>
-    </div>
+                    <tbody class="app__tbody">
+                        <!---LOOP THROUGH AND DISPLAY USER -->
+                        <?php while ($categories = mysqli_fetch_assoc(($categories))) : ?>
+                            <tr clas="app__tr">
+                                <td class="app__td"><?= $categories['title']  ?></td>
+                                <td class="app__td"><?= $categories['description'] ?? null ?></td>
+                                <td class="app__td"><a href="<?= ADMIN_URL ?>edit_category.php?id=<?= $categories['id'] ?>" class="app__link-btn">Edit</a></td>
+                                <td class="app__td"><a href="<?= ADMIN_URL ?>delete_category.php?id=<?= $categories['id'] ?>" class="app__alert-btn-sm">Delete</a>
+                                </td>
+                            </tr>
+                        <?php endwhile; ?>
+                    </tbody>
+                </table>
+            </div>
 
 
+        <?php else : ?>
+            <div class="app__alert-error">
+                <p class="app__alert-p">
+                    <?= " No Catgories Found !" ?>
 
-    <?php endif; ?>
+                </p>
+            </div>
 
-
-
-    <!--- Table and While Loop End -->
+        <?php endif; ?>
+        <!--- Table and While Loop End -->
     <?php endif; ?>
     <!-- if @uth End -->
 </section>
 
 
 <?php
-
 include('./partials/footer/footer.php');
 ?>
