29936ae5884d5eb38b1c002b9778358840df18e8
omarashzeinhom <omar.ashraf.zeinhom@hotmail.com>
2022-09-09 02:27:33 +0200

    added-product-section

---
+++ adminpanel/manage_products.php
@@ -0,0 +1,66 @@
+<?php
+$page__title = 'Manage Products🛍️ ';
+include('./partials/sidenav/sidenav.php');
+
+
+$query = "SELECT * FROM products";
+$products = mysqli_query($connect__db, $query);
+$queryc = "SELECT * FROM categories";
+$categories = mysqli_query($connect__db, $queryc);
+
+?>
+
+
+<h1><?php echo $page__title; ?></h1>
+
+
+<ul class='app__sidenav-items'>
+    <li class='app__sidenav-item'><a class="app__link-btn" href="<?= ADMIN_URL ?>add_product.php">Add
+            Product🛍️ </a></li>
+</ul>
+<!-- Add Post Alert success Start -->
+<?php if (isset($SESSION_['add-post-success'])) : ?>
+    <div class="app__alert-success">
+        <p class="app__alert-success-p">
+            <?= $_SESSION['add-post-success'];
+            unset($_SESSION['add-post-success']); ?>
+        </p>
+    </div>
+<?php endif; ?>
+<!-- Add Post Alert success End  -->
+
+<table class="app__table" style="overflow: x-auto; margin: auto; max-width: 1700px; width:100%; height:100%;">
+    <thead class="app__thead">
+        <tr clas="app__tr">
+            <th class="app_th">Title</th>
+            <th class="app_th">Product</th>
+            <th class="app_th">Thumbnail</th>
+            <th class="app_th">Price</th>
+            <th class="app_th">Edit</th>
+            <th class="app_th">Delete</th>
+        </tr>
+    </thead>
+    <tbody class="app__tbody">
+        <?php while ($single_product = mysqli_fetch_assoc($products)) : ?>
+            <!---LOOP THROUGH AND DISPLAY POSTS -->
+            <tr clas="app__tr">
+                <td class="app__td" id="postsTitle"><?= $single_product['title']; ?></td>
+                <!--- Posts Title  -->
+                <td class="app__td" id="postsBody"> <?= substr($single_product['body'], 0, 50)  . " ..."; ?></td>
+                <!--- Posts Body -->
+                <td class="app__td" id="postsThumbnail"> <img src="<?= ROOT_URL . 'images/' . $single_product['img'] ?>" style="padding-top: 0.4rem;border-radius: 15%; object-fit:cover; box-shadow: 0.1rem 0.1rem 0.1rem 0.1rem gray;" class="app__thumbnail-avatar" width="50px" height="50px" alt="admin_post_thumbnail" /></td>
+
+                <!--- TODO: FIX Posts Thumbnail -->
+                <td class="app__td" id="postsFeatured"><input type="checkbox" value="" checked disabled /></td>
+                <!--- Posts Featured -->
+                <td class="app__td"><a href="<?= ADMIN_URL ?>edit_post.php?id=<?= $single_post['id'] ?? null; ?>" class="app__link-btn">Edit</a></td>
+                <td class="app__td"><a href="<?= ADMIN_URL ?>delete_post.php?id=<?= $single_post['id'] ?? null; ?>" class="app__alert-btn-sm">Delete</a></td>
+            </tr>
+        <?php endwhile; ?>
+    </tbody>
+</table>
+
+<?php
+
+include('./partials/footer/footer.php');
+?>
--- adminpanel/partials/dashboard/dashboard.php
+++ adminpanel/partials/dashboard/dashboard.php
@@ -22,17 +22,22 @@ include('./partials/sidenav/sidenav.php');
         <!--TODO: RESTRICT SIDE NAV ITEMS IF NOT ADMIN - RESTRICT USERS AND CATEGORIES -->
         <?php
         if (isset($_SESSION['user_is_admin'])) : ?>
-        <div class="app__dashboard-icon">
-            <a href="<?= HOME_URL ?>manage_users.php" class="app__dashboard-link">
-                👥 Users
-            </a>
-        </div>
+            <div class="app__dashboard-icon">
+                <a href="<?= HOME_URL ?>manage_users.php" class="app__dashboard-link">
+                    👥 Users
+                </a>
+            </div>
 
-        <div class="app__dashboard-icon">
-            <a href="<?= HOME_URL ?>manage_categories.php" class="app__dashboard-link">
-                ✨ Categories
-            </a>
-        </div>
+            <div class="app__dashboard-icon">
+                <a href="<?= HOME_URL ?>manage_categories.php" class="app__dashboard-link">
+                    ✨ Categories
+                </a>
+            </div>
+            <div class="app__dashboard-icon">
+                <a href="<?= HOME_URL ?>manage_products.php" class="app__dashboard-link">
+                    🛍️ Shop
+                </a>
+            </div>
         <?php endif; ?>
 
     </div>
