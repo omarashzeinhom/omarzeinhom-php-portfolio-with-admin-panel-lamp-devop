06048eb5d5e910770e95b7c112c8ec52593ddef7
omarashzeinhom <omar.ashraf.zeinhom@hotmail.com>
2022-09-09 03:23:46 +0200

    added-add-product-logic-still-in-development

--- adminpanel/add_product-logic.php
+++ adminpanel/add_product-logic.php
@@ -10,16 +10,14 @@ if (isset($_POST['submit__newuser'])) {
     $description = filter_var($_POST['description'], FILTER_SANITIZE_FULL_SPECIAL_CHARS);
     $price = filter_var($_POST['email'], FILTER_VALIDATE_EMAIL);
     $retailprice = filter_var($_POST['retailprice'], FILTER_VALIDATE_EMAIL);
-
+    $quantity = filter_var($_POST['user__role'], FILTER_SANITIZE_NUMBER_INT);
     $image = $_FILES['image'];
-    //user role 
-    $is_admin = filter_var($_POST['user__role'], FILTER_SANITIZE_NUMBER_INT);
 
 
     //DEBUG Inputs
     //echo $firstname, $lastname, $username, $email, $password, $confirmpassword;
     //DEBUG Avatar Var
-    // var_dump($avatar);
+    var_dump($image);
 
     //VALIDATING Input Values
     if (!$title) {
@@ -32,46 +30,41 @@ if (isset($_POST['submit__newuser'])) {
         $_SESSION['add-product'] = "Your Retail Price is missing ⁉️";
     } else {
         //check if confirm password matches the createdpassword
-        if ($createpassword !== $confirmpassword) {
-            $_SESSION['add-product'] = 'Passwors Entered do not match';
-        } else {
-            //hashed password
-            $hashed_pass =  password_hash($createpassword, PASSWORD_DEFAULT);
-            //echo $createpassword . '<br/>';
-            //echo $hashed_pass;
-            //check if username or the email already exists in the database
-            $username_check_query = "SELECT * FROM users WHERE
-            username = '$username' OR email='$email'";
-            $username_check_results = mysqli_query($connect__db, $username_check_query);
-            if (mysqli_num_rows($username_check_results) > 0) {
-                $_SESSION['add-product'] = 'UserName or Email already exists';
-            } else {
-                // adduser avatar picture name to be unique
-                $time = time();
-                $avatar_new_name = $time . $avatar['name'];
-                $avatar_tmp_name = $avatar['tmp_name'];
-                $avatar_destination_path = '../images/' . $avatar_new_name;
 
-                //DEBUG 
-                // if file is an image
-                //allowed_images files 
-                $allowed_images = ['png', 'jpg', 'jpeg', 'svg', 'webp'];
-                $extension = explode('.', $avatar_new_name);
-                $extension = end($extension);
-                if (in_array($extension, $allowed_images)) {
-                    //CHECK : if image size is not too large (500kb+)
-                    if ($avatar['size'] < 500000) /*avatar size is less than 500k bytes = 0.5 mb  */ {
-                        //UPLOAD AVATAR
-                        move_uploaded_file($avatar_tmp_name, $avatar_destination_path);
-                    } else {
-                        $_SESSION['add-product'] = 'File Size is too Big Please Use an Image converter to make the image below 0.5mb , since it differs in the websites performance ';
-                    }
+
+        //check if username or the email already exists in the database
+        $product_check_query = "SELECT * FROM products WHERE
+            title = '$title' OR description='$description'";
+        $product_check_results = mysqli_query($connect__db, $product_check_query);
+        if (mysqli_num_rows($product_check_results) > 0) {
+            $_SESSION['add-product'] = 'Product already exists';
+        } else {
+            // adduser avatar picture name to be unique
+            $time = time();
+            $image_new_name = $time . $image['image'];
+            $imagetmp_name = $product['tmp_name'];
+            $image_destination_path = '../images/' . $image_new_name;
+
+            //DEBUG 
+            // if file is an image
+            //allowed_images files 
+            $allowed_images = ['png', 'jpg', 'jpeg', 'svg', 'webp'];
+            $extension = explode('.', $product_new_name);
+            $extension = end($extension);
+            if (in_array($extension, $allowed_images)) {
+                //CHECK : if image size is not too large (500kb+)
+                if ($product['size'] < 500000) /*avatar size is less than 500k bytes = 0.5 mb  */ {
+                    //UPLOAD AVATAR
+                    move_uploaded_file($product_tmp_name, $product_destination_path);
                 } else {
-                    $_SESSION['add-product'] = 'Files Accepted are jpeg, png, jpg , .svg and .webp';
+                    $_SESSION['add-product'] = 'File Size is too Big Please Use an Image converter to make the image below 0.5mb , since it differs in the websites performance ';
                 }
+            } else {
+                $_SESSION['add-product'] = 'Files Accepted are jpeg, png, jpg , .svg and .webp';
             }
         }
     }
+
 
     //DEBUG AVATAR Using var_dump();
     //var_dump($avatar);
@@ -83,13 +76,13 @@ if (isset($_POST['submit__newuser'])) {
         die();
     } else {
         //insert new user into users table 
-        $insert_user_query = "INSERT INTO users SET firstname='$firstname', lastname='$lastname', username='$username', email='$email', password='$hashed_pass', avatar='$avatar_new_name', is_admin=$is_admin ";
+        $insert_user_query = "INSERT INTO products SET title='$title', description='$description', price=$price, retailprice=$retailprice, quantity=$quantity image='$image'";
         //passing mysqli_query using the insert userquery
         $insert_user_results = mysqli_query($connect__db, $insert_user_query);
         if (!mysqli_errno($connect__db)) {
             //redirecting to login page
-            $_SESSION['add-product-success'] = "A new user $firstname $lastname has been added successfully";
-            header('location:' . ADMIN_URL . 'manage_users.php');
+            $_SESSION['add-product-success'] = "A new product $title $lastname has been added successfully";
+            header('location:' . ADMIN_URL . 'manage_products.php');
             die();
         }
     }
