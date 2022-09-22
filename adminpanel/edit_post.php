da44f96f87fcd4e827873c11aef7d17aaac95163
omarashzeinhom <omar.ashraf.zeinhom@hotmail.com>
2022-09-22 20:20:16 +0200

    added-gui-fix-for-categories-table

--- adminpanel/edit_post.php
+++ adminpanel/edit_post.php
@@ -28,18 +28,16 @@ if (isset($_GET['id'])) {
 
 
 <section style="overflow: x-auto; margin: auto; max-width: 500px; width:100%; height:100%;">
-    <form class="app__form-section" action="<?= HOME_URL ?>edit_post-logic.php" method="POST"
-        enctype="multipart/form-data">
+    <form class="app__form-section" action="<?= HOME_URL ?>edit_post-logic.php" method="POST" enctype="multipart/form-data">
         <select class="app__input" name="selectrole__edituser">
-
+            <!--- While Loop For Category Start  --->
             <?php while ($single_category = mysqli_fetch_assoc($categories)) : ?>
-            <?php
-
+                <?php
                 echo ($post['id']);; ?>
-            <option value="<?= $single_category['id']; ?>"><?= $single_category['title']; ?></option>
-
+                <option value="<?= $single_category['id']; ?>"><?= $single_category['title']; ?></option>
             <?php endwhile; ?>
         </select>
+        <!--- While Loop For Category End  --->
 
         <!--- HIDDEN ID --->
         <div class="app__inputs-wrap">
@@ -48,34 +46,31 @@ if (isset($_GET['id'])) {
         <!-- Title  --->
         <div class="app__inputs-wrap">
             <label class="app__inputs-label" for="title">Title</label>
-            <input name="title" class="app__input" type="text" id="firstName" value="<?= $post['title'] ?>"
-                placeholder="Enter Post Title here..." />
+            <input name="title" class="app__input" type="text" id="firstName" value="<?= $post['title'] ?>" placeholder="Enter Post Title here..." />
         </div>
-        <!-- Post Text --->
+        <!-- Post Body Start  --->
         <div class="app__inputs-wrap">
             <label class="app__inputs-label" for="body">Body</label>
-            <textarea name="body" class="app__input" style="resize:none; " rows="8" value="<?= $post['body'] ?>"
-                placeholder="test">
+            <textarea name="body" class="app__input" style="resize:none; " rows="8" value="<?= $post['body'] ?>" placeholder="test">
                 <?= $post['body'] ?>
                 </textarea>
         </div>
-
+        <!-- Post Body End   --->
+        <!-- Post Image Start  --->
+        <small>TODO: Edit Thumbnmail For Post</small>
         <div class="app__inputs-wrap">
             <label class="app__inputs-label" for="title">Thumbnail</label>
             <img src="<?= ROOT_URL . 'images/' . $post['thumbnail'] ?>" class="app__thumbnail-avatar" />
-            <input name="thumbnail" class="app__input" type="file" id="firstName"
-                value="<?= ROOT_URL . 'images/' . $post['thumbnail'] ?>" placeholder="Enter Post Thumbnail here." />
+            <input name="thumbnail" class="app__input" type="file" id="firstName" value="<?= ROOT_URL . 'images/' . $post['thumbnail'] ?>" placeholder="Enter Post Thumbnail here." />
         </div>
-
+        <!-- Post Image End --->
+        <!-- Post Featured Start  --->
         <div class="app__inputs-wrap">
             <label class="app__inputs-label" for="body">Featured</label>
             <input type="checkbox" value="<?= $post['is_featured']; ?>" checked />
         </div>
-
+        <!-- Post Featured End  --->
         <button name="submit__editpost" type="submit" class="btn">Update Post</button>
-
-
-
     </form>
 </section>
 
