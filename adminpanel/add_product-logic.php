b863851ca947263515c9311004596326f4b87257
omarashzeinhom <omar.ashraf.zeinhom@hotmail.com>
2022-09-22 20:39:04 +0200

    added-products-and-displayed-them-in-manage-products.php-still-wip

--- adminpanel/add_product-logic.php
+++ adminpanel/add_product-logic.php
@@ -12,83 +12,63 @@ if (isset($_POST['submit__newproduct'])) {
     $retailprice = filter_var($_POST['retailprice'], FILTER_SANITIZE_NUMBER_FLOAT);
     $quantity = filter_var($_POST['quantity'], FILTER_SANITIZE_NUMBER_FLOAT);
     $img = $_FILES['img'];
-    var_dump($title);
+    var_dump($name, $description, $price, $retailprice, $quantity, $img);
 
     //DEBUG Inputs
-    echo $name, $description, $price, $retailprice, $quantity, $img;
     //DEBUG Avatar Var
-    var_dump($image);
 
-    //VALIDATING Input Values
+    //SET is_featured to 0 if unchecked 
+
     if (!$name) {
-        $_SESSION['add-product'] = "Your Product Title is missing ⁉️";
+        $_SESSION['add-product'] = "Kindly ,Enter The Product  Title";
     } elseif (!$description) {
-        $_SESSION['add-product'] = "Your Description is missing ⁉️";
-    } elseif (!$price) {
-        $_SESSION['add-product'] = "Your Price is missing ⁉️";
-    } elseif (!$retailprice) {
-        $_SESSION['add-product'] = "Your Retail Price is missing ⁉️";
-    } elseif (!$name || !$description || !$price || !$retailprice) {
-        $_SESSION['add-product'] = "Your Product info is missing please add it in again ⁉️";
-    }
-
-    $product_check_query = "SELECT * FROM products WHERE
-            name= '$name' OR description='$description'";
-    $product_check_results = mysqli_query($connect__db, $product_check_query);
-    if (mysqli_num_rows($product_check_results) > 0) {
-        $_SESSION['add-product'] = 'Product already exists';
+        $_SESSION['add-productt'] = "Kindly ,Enter The Description for the product";
+    } elseif (!$img['name']) {
+        $_SESSION['add-product'] = "Kindly ,Upload A Thumbnail For Your Product";
     } else {
-        // adduser avatar picture name to be unique
-        $time = time();
-        $img_new_name = $time . $img['img'];
-        $imgtmp_name = $product['tmp_name'];
-        $img_destination_path = '../images/' . $img_new_name;
+        //RENAME IMAGES TO NOT HAVE DUPLICATES
+        $time = time(); //USED TO MAKE EACH IMAGE UNIQUE
+        $thumbnail_newname = $time . $img['name'];
+        //var_dump($thumbnail_newname);
+        $thumbnail_tmp_name = $img['tmp_name'];
+        $thumbnail__destination_path =  '../images/' . $thumbnail_newname;
 
-        //DEBUG 
-        // if file is an image
-        //allowed_images files 
-        $allowed_images = ['png', 'jpg', 'jpeg', 'svg', 'webp'];
-        $extension = explode('.', $img_new_name);
-        $extension = end($extension);
-        if (in_array($extension, $allowed_images)) {
-            //CHECK : if image size is not too large (500kb+)
-            if ($product['size'] < 500000) /*avatar size is less than 500k bytes = 0.5 mb  */ {
+        //CHECK IF FILE IS AN IMG 
+
+        $allowed_thumbnails = ['png', 'jpg', 'jpeg', 'svg', 'webp', 'PNG'];
+        $extension = explode('.', $thumbnail_newname);
+        $extension = end($extension); // CHECK  AND COMPARE END OF ARRAY
+        var_dump($extension);
+        if (in_array($extension, $allowed_thumbnails)) {
+            //CHECK : if image size is not too large (1mb+)
+            if ($img['size'] < 5_00_000) /*avatar size is bigger than 500k bytes = 0.5 mb  */ {
                 //UPLOAD AVATAR
-                move_uploaded_file($product_tmp_name, $product_destination_path);
+                move_uploaded_file($thumbnail_tmp_name, $thumbnail__destination_path);
             } else {
                 $_SESSION['add-product'] = 'File Size is too Big Please Use an Image converter to make the image below 0.5mb , since it differs in the websites performance ';
             }
         } else {
             $_SESSION['add-product'] = 'Files Accepted are jpeg, png, jpg , .svg and .webp';
         }
-    }
+    };
 
 
-
-    //DEBUG AVATAR Using var_dump();
-    //var_dump($avatar);
     if (isset($_SESSION['add-product'])) {
-        // passing the form data back to the add_user page
-        //sends all invalid details to add_user page make sure its the session add-user-data
-        $_SESSION['add-product'] = $_POST;
-        header('location:' . ADMIN_URL . 'add_product.php');
+        $_SESSION['product-data'] = $_POST;
+        header("location:" . ADMIN_URL . "add_product.php");
         die();
     } else {
-        //insert new user into users table 
-        $insert_product_query = "INSERT INTO products SET name='$name', description='$description', price=$price, retailprice=$retailprice, quantity=$quantity image='$img_new_name'";
-        //passing mysqli_query using the insert userquery
-        $insert_user_results = mysqli_query($connect__db, $insert_product_query);
+        //insert new posts into posts table 
+        $insert_products_query = "INSERT INTO products SET name='$name', description='$description', price=$price, retailprice=$retailprice=', quantity=$quantity, img=$thumbnail_newname'";
+        //passing mysqli_query using the insert_posts_query
+        $insert_products_results = mysqli_query($connect__db, $insert_products_query);
         if (!mysqli_errno($connect__db)) {
             //redirecting to login page
-            $_SESSION['add-product-success'] = "A new product $name has been added successfully";
+            $_SESSION['add-product-success'] = "Products Added Successfully ! please Login In";
             header('location:' . ADMIN_URL . 'manage_products.php');
             die();
         }
-    }
-} else {
-    header('location:' . ADMIN_URL . 'manage_products.php');
-
-    die();
+    };
 }
 
 //TODO Fix or Change LOGIC 
