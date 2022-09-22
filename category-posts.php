b1db5018fc8b28924fad239eccdadcf4eef1ef28
omarashzeinhom <omar.ashraf.zeinhom@hotmail.com>
2022-09-22 06:22:52 +0200

    reformatted-docs

--- category-posts.php
+++ category-posts.php
@@ -1,4 +1,7 @@
 <?php
-$page__title = 'Posts Category'; ?>
+$page__title = 'Posts Category';
+include('./partials/header/header.php');
+include('./partials/nav/nav.php');
+?>
 
 <h1><?php echo "$page__title" ?> </h1>
--- single-post.php
+++ single-post.php
@@ -1,4 +1,6 @@
 <?php
+include('./partials/header/header.php');
+include('./partials/nav/nav.php');
 $page__title = 'Single Post'; ?>
 
 <h1><?php echo "$page__title" ?> </h1>
