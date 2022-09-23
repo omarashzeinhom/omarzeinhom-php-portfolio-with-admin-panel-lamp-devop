96c36935f5d48762622926304df51396db37e79a
omarashzeinhom <omar.ashraf.zeinhom@hotmail.com>
2022-09-23 06:30:33 +0200

    fixed-weird-error-in-nav-in-main-project

--- category-posts.php
+++ category-posts.php
@@ -25,77 +25,76 @@ $category_title = $category['title'];
 <h1> <?= $category_title ?? null; ?>
 </h1>
 <!--- Categories -->
-<?php if (mysqli_num_rows($result) >= 1) : ?>
-    <?php while ($post = mysqli_fetch_assoc($result)) : ?>
-        <div class="app__col">
-            <!-- Card Start --->
-            <div class="app__card" id="appCardId">
-                <div class="app__card-header">
-                    <div class="app__card-img-shadow">
-                        <a href="<?= HOME_URL ?>single-post.php?id=<?= $post['id'] ?>">
-                            <img src="<?= HOME_URL . 'images/' . $post['thumbnail'] ?>" class="app__card-img" alt="admin_post_thumbnail" loading="lazy" />
-                        </a>
-                    </div>
-                    <h5 id="postsTitle"> <a href="<?= HOME_URL ?>single-post.php?id=<?= $post['id'] ?>">
-                            <?= $post['title']; ?>
-                        </a>
+<?php while ($post = mysqli_fetch_assoc($result)) : ?>
+<div class="app__col">
+    <!-- Card Start --->
+    <div class="app__card" id="appCardId">
+        <div class="app__card-header">
+            <div class="app__card-img-shadow">
+                <a href="<?= HOME_URL ?>single-post.php?id=<?= $post['id'] ?>">
+                    <img src="<?= HOME_URL . 'images/' . $post['thumbnail'] ?>" class="app__card-img"
+                        alt="admin_post_thumbnail" loading="lazy" />
+                </a>
+            </div>
+            <h5 id="postsTitle"> <a href="<?= HOME_URL ?>single-post.php?id=<?= $post['id'] ?>">
+                    <?= $post['title']; ?>
+                </a>
+            </h5>
+            <!--- Posts Title  -->
+            <h6 class="app__td" id="postsBody" maxlength="100">
+                <?= substr($post['body'], 0, 100)  . " ..."; ?>
+            </h6>
+        </div>
+        <div class="app__card-footer">
+            <!-- Card Footer Start --->
+            <!--- Posts End -->
+            <div class="app__card-author">
+                <?php
+                    //FETCH THE AUTHOR FROM USERS TABLE USING AUTHOR_ID
+                    $author_id = $post['author_id'];
+                    $author_query = "SELECT * FROM users WHERE id=$author_id";
+                    $author_result = mysqli_query($connect__db, $author_query);
+                    $author = mysqli_fetch_assoc($author_result);
+                    ?>
+                <strong>
+                    <h5>
+                        By: <?= "{$author['firstname']} {$author['lastname']}"; ?>
                     </h5>
-                    <!--- Posts Title  -->
-                    <h6 class="app__td" id="postsBody" maxlength="100">
-                        <?= substr($post['body'], 0, 100)  . " ..."; ?>
-                    </h6>
-                </div>
-                <div class="app__card-footer">
-                    <!-- Card Footer Start --->
-                    <!--- Posts End -->
-                    <div class="app__card-author">
-                        <?php
-                        //FETCH THE AUTHOR FROM USERS TABLE USING AUTHOR_ID
-                        $author_id = $post['author_id'];
-                        $author_query = "SELECT * FROM users WHERE id=$author_id";
-                        $author_result = mysqli_query($connect__db, $author_query);
-                        $author = mysqli_fetch_assoc($author_result);
-                        ?>
-                        <strong>
-                            <h5>
-                                By: <?= "{$author['firstname']} {$author['lastname']}"; ?>
-                            </h5>
-                            <img src="./images/<?= $author['avatar'] ?>" class="app__nav-avatar" />
-                        </strong>
-                        <small>
-                            <em>
-                                Posted At: <?= date("F j, Y, G:i:s ", strtotime($post['date_time'])) ?>
-                            </em>
-                        </small>
-                    </div>
-                </div><!-- Card Footer End--->
-            </div><!-- Card End--->
-        </div>
-        </div>
-        <!-- Btn Wrap  End--->
-        </div> <!-- Column End--->
-        <!--- All Categories Start --->
-        <div class="app__container">
-            <?php
-
-            $all_categories_query = "SELECT * FROM categories";
-            $all_categories_result = mysqli_query($connect__db, $all_categories_query);
-
-            ?>
-            <?php
-
-            while ($category = mysqli_fetch_assoc($all_categories_result)) :
-            ?>
-                <button class="btn__sm">
-                    <a href="<?= HOME_URL ?>category-posts.php?id=<?= $category['id'] ?>">
-                        <?= $category['title'] ?? null; ?>
-                    </a>
-
-                </button>
-            <?php endwhile; ?>
-        </div>
-        <!--- All Categories End --->
+                    <img src="./images/<?= $author['avatar'] ?>" class="app__nav-avatar" />
+                </strong>
+                <small>
+                    <em>
+                        Posted At: <?= date("F j, Y, G:i:s ", strtotime($post['date_time'])) ?>
+                    </em>
+                </small>
+            </div>
+        </div><!-- Card Footer End--->
+    </div><!-- Card End--->
+</div>
+</div>
+<!-- Btn Wrap  End--->
+</div> <!-- Column End--->
+<?php endwhile; ?>
 
 
+<!--- All Categories Start --->
+<div class="app__container">
+    <?php
+
+    $all_categories_query = "SELECT * FROM categories";
+    $all_categories_result = mysqli_query($connect__db, $all_categories_query);
+
+    ?>
+    <?php
+
+    while ($category = mysqli_fetch_assoc($all_categories_result)) :
+    ?>
+    <button class="btn__sm">
+        <a href="<?= HOME_URL ?>category-posts.php?id=<?= $category['id'] ?>">
+            <?= $category['title'] ?? null; ?>
+        </a>
+
+    </button>
     <?php endwhile; ?>
-<?php endif; ?>
+</div>
+<!--- All Categories End --->
--- single-post.php
+++ single-post.php
@@ -14,38 +14,60 @@ if (isset($_GET['id'])) {
 }
 $page__title = $post['title'];
 ?>
+<div class="app__card-lg">
+    <h1><?php echo $page__title; ?></h1>
+    <img src="<?= HOME_URL . 'images/' . $post['thumbnail'] ?>" class="app__card-img" alt="<?= $post['title']; ?>"
+        loading="lazy" />
+    <div class="app__card-author">
+        <?php
+        //FETCH THE AUTHOR FROM USERS TABLE USING AUTHOR_ID
+        $author_id = $post['author_id'];
+        $author_query = "SELECT * FROM users WHERE id=$author_id";
+        $author_result = mysqli_query($connect__db, $author_query);
+        $author = mysqli_fetch_assoc($author_result);
+        ?>
+        <strong>
 
-<h1><?php echo $page__title; ?></h1>
+            <h5>
+                By: <?= "{$author['firstname']} {$author['lastname']}"; ?>
+            </h5>
+            <img src="./images/<?= $author['avatar'] ?>" class="app__nav-avatar" alt="<?= $author['avatar'] ?>" />
+        </strong>
+        <small>
+            <em>
+                Posted At: <?= date("F j, Y, G:i:s ", strtotime($post['date_time'])) ?>
+            </em>
+        </small>
+
+    </div>
 
 
-<img src="<?= HOME_URL . 'images/' . $post['thumbnail'] ?>" class="app__card-img" alt="<?= $post['title']; ?>"
-    loading="lazy" />
+    <p style="font-size:medium;">
+        <?= $post['body']; ?>
+    </p>
 
-<div class="app__card-author">
-    <?php
-    //FETCH THE AUTHOR FROM USERS TABLE USING AUTHOR_ID
-    $author_id = $post['author_id'];
-    $author_query = "SELECT * FROM users WHERE id=$author_id";
-    $author_result = mysqli_query($connect__db, $author_query);
-    $author = mysqli_fetch_assoc($author_result);
-    ?>
-
-    <strong>
-
-        <h5>
-            By: <?= "{$author['firstname']} {$author['lastname']}"; ?>
-        </h5>
-        <img src="./images/<?= $author['avatar'] ?>" class="app__nav-avatar" alt="<?= $author['avatar'] ?>" />
-    </strong>
-    <small>
-        <em>
-            Posted At: <?= date("F j, Y, G:i:s ", strtotime($post['date_time'])) ?>
-        </em>
-    </small>
 
 </div>
 
 
-<p>
-    <?= $post['body']; ?>
-</p>
+<!--- All Categories Start --->
+<div class="app__container">
+    <?php
+
+    $all_categories_query = "SELECT * FROM categories";
+    $all_categories_result = mysqli_query($connect__db, $all_categories_query);
+
+    ?>
+    <?php
+
+    while ($category = mysqli_fetch_assoc($all_categories_result)) :
+    ?>
+    <button class="btn__sm">
+        <a href="<?= HOME_URL ?>category-posts.php?id=<?= $category['id'] ?>">
+            <?= $category['title'] ?? null; ?>
+        </a>
+
+    </button>
+    <?php endwhile; ?>
+</div>
+<!--- All Categories End --->
