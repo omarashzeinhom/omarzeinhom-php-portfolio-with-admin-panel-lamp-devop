e5d7e053c0f7e47683e99f3bc4b3839fce076b97
omarashzeinhom <omar.ashraf.zeinhom@hotmail.com>
2022-09-24 03:40:07 +0200

    added-fixes-for-html-gui-in/partials/homeposts/homeposts.hphp

--- category-posts.php
+++ category-posts.php
@@ -27,66 +27,68 @@ $category_title = $category['title'];
 <h2>
     <?= $category_title; ?>
 </h2>
-<!--- Posts Category -->
 
-<h2><?= $category_post['title']; ?></h2>
-<div class="app__card-footer">
-    <!-- Card Footer Start --->
-    <img src="<?= HOME_URL . 'images/' . $category_post['thumbnail'] ?>" class="app__card-img"
-        alt="admin_post_thumbnail" loading="lazy" />
+<!-- Column Start --->
 
+<div class="app__col">
+    <!-- Card Start --->
+    <div class="app__card" id="appCardId">
+        <!-- Card Header Start --->
+        <div class="app__card-header">
+            <h2><?= $category_post['title']; ?></h2>
+        </div>
+        <!-- Card Header End--->
+        <img src="<?= HOME_URL . 'images/' . $category_post['thumbnail'] ?>" class="app__card-img" alt="admin_post_thumbnail" loading="lazy" />
 
-    <button class="btn__sm">
-        <a href="<?= HOME_URL ?>category-posts.php?id=<?= $category['id'] ?>">
-            <?= $category_title ?? null; ?>
-        </a>
-    </button>
-    <!--- Posts End -->
-    <div class="app__card-author">
-        <?php
-        //FETCH THE AUTHOR FROM USERS TABLE USING AUTHOR_ID
-        $author_id = $category_post['author_id'];
-        $author_query = "SELECT * FROM users WHERE id=$author_id";
-        $author_result = mysqli_query($connect__db, $author_query);
-        $author = mysqli_fetch_assoc($author_result);
-        ?>
+        <!-- Card Footer Start --->
+        <div class="app__card-footer">
+            <button class="btn__sm">
+                <a href="<?= HOME_URL ?>category-posts.php?id=<?= $category['id'] ?>">
+                    <?= $category_title ?? null; ?>
+                </a>
+            </button>
+            <!--- Posts End -->
+            <!-- Card Author Start--->
+            <div class="app__card-author">
+                <?php
+                //FETCH THE AUTHOR FROM USERS TABLE USING AUTHOR_ID
+                $author_id = $category_post['author_id'];
+                $author_query = "SELECT * FROM users WHERE id=$author_id";
+                $author_result = mysqli_query($connect__db, $author_query);
+                $author = mysqli_fetch_assoc($author_result);
+                ?>
+                <strong>
+                    <h5>
+                        By: <?= "{$author['firstname']} {$author['lastname']}"; ?>
+                    </h5>
+                    <img src="./images/<?= $author['avatar'] ?>" class="app__nav-avatar" />
+                </strong>
+                <small>
+                    <em>
+                        Posted At: <?= date("F j, Y, G:i:s ", strtotime($category_post['date_time'])) ?>
+                    </em>
+                </small>
+            </div><!-- Card Author End--->
+        </div><!-- Card Footer End--->
 
-        <strong>
+    </div> <!-- Card End--->
 
-            <h5>
-                By: <?= "{$author['firstname']} {$author['lastname']}"; ?>
-            </h5>
-            <img src="./images/<?= $author['avatar'] ?>" class="app__nav-avatar" />
-        </strong>
-        <small>
-            <em>
-                Posted At: <?= date("F j, Y, G:i:s ", strtotime($category_post['date_time'])) ?>
-            </em>
-        </small>
-
-    </div>
-
-</div><!-- Card Footer End--->
-
+</div> <!-- Column End--->
 
 <!--- All Categories Start --->
 <div class="app__container">
     <?php
-
     $all_categories_query = "SELECT * FROM categories";
     $all_categories_result = mysqli_query($connect__db, $all_categories_query);
-
     ?>
     <?php
-
     while ($category = mysqli_fetch_assoc($all_categories_result)) :
     ?>
-    <button class="btn__sm">
-        <a href="<?= HOME_URL ?>category-posts.php?id=<?= $category['id'] ?>">
-            <?= $category['title'] ?? null; ?>
-        </a>
-
-    </button>
+        <button class="btn__sm">
+            <a href="<?= HOME_URL ?>category-posts.php?id=<?= $category['id'] ?>">
+                <?= $category['title'] ?? null; ?>
+            </a>
+        </button>
     <?php endwhile; ?>
 </div>
 <!--- All Categories End --->
